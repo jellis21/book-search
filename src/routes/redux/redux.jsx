7a2855337b916c75ac94c86d6124921c4a727a9e
Joshua Ellis <joshua.ellis.r@gmail.com>
2021-12-09 19:03:52 -0500

    save list with one friend

--- src/routes/redux/redux.jsx
+++ src/routes/redux/redux.jsx
@@ -1,19 +1,28 @@
 import { connect } from "react-redux";
+import { useState } from 'react';
 import getNewFriends from "../../redux/actions/getNewFriends";
 import "./style.css";
 
 function Redux({ friends, getNewFriends }) {
+  
+  const [newFriend, setNewFriend] = useState(null);
+
   const amigos = friends.map((friend, index) => {
     return <li key={index}>{friend}</li>;
   });
 
   const handleClick = () => {
-    getNewFriends()
+    getNewFriends([newFriend]);
+  }
+
+  const handleChange = e => {
+    setNewFriend(e.target.value);
   }
 
   return (
     <div>
       <ul>{amigos}</ul>
+      <input type="text" onChange={handleChange} />
       <button onClick={handleClick}>Get New Friends</button>
     </div>
   );
